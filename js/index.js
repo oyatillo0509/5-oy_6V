f539aeb06ca760aaba9ef1e5bfd22a282592eb18
oyatillo0509 <tilloxon0509@gmail.com>
2024-06-28 13:35:59 +0500

    tugallandi

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.getElementById("signUpForm").addEventListener("submit", function (e) {
+document.getElementById("sign").addEventListener("submit", function (e) {
   e.preventDefault();
 
   const f_name = document.getElementById("firstName").value;
@@ -6,13 +6,23 @@ document.getElementById("signUpForm").addEventListener("submit", function (e) {
   const p_num = document.getElementById("mobileNumber").value;
   const email = document.getElementById("email").value;
   const table = document
-
     .getElementById("t_table")
     .getElementsByTagName("tbody")[0];
   const new_t = table.insertRow();
 
-  new_t.insertCell(0).appendChild(document.createTextNode(f_name));
-  new_t.insertCell(1).appendChild(document.createTextNode(l_name));
-  new_t.insertCell(2).appendChild(document.createTextNode(p_num));
-  new_t.insertCell(3).appendChild(document.createTextNode(email));
+  const f_name_td = document.createElement("td");
+  f_name_td.innerHTML = f_name;
+  new_t.appendChild(f_name_td);
+
+  const l_name_td = document.createElement("td");
+  l_name_td.innerHTML = l_name;
+  new_t.appendChild(l_name_td);
+
+  const p_num_td = document.createElement("td");
+  p_num_td.innerHTML = p_num;
+  new_t.appendChild(p_num_td);
+
+  const email_td = document.createElement("td");
+  email_td.innerHTML = email;
+  new_t.appendChild(email_td);
 });
